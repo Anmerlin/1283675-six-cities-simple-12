bbe8ad6142ea076d5a31f34a225addaece8779ce
Andrey Sigarenko <sigarenko@bk.ru>
2023-03-04 10:48:36 +0300

    Добавляет React-компоненты

---
+++ project/src/pages/main/main.tsx
@@ -0,0 +1,13 @@
+import Header from '../../components/header/header';
+import MainContent from '../../components/main/main';
+
+function MainScreen(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+}
+
+export default MainScreen;
